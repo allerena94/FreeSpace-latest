0a6079c0d3ddcb57e4d7a3722d800bf059697416
Andy Llerena <allerena@villanova.edu>
2016-04-23 19:17:08 -0400

    Initial commit

---
+++ app/src/main/java/villanova/freespace/LoginActivity.java
@@ -0,0 +1,66 @@
+package villanova.freespace;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText user;
+    EditText pass;
+    TextView login;
+    TextView signup;
+
+    String sUser;
+    String sPass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Typeface custom_font = Typeface.createFromAsset(getAssets(), "fonts/LatoLight.ttf");
+        Typeface custom_font1 = Typeface.createFromAsset(getAssets(), "fonts/LatoRegular.ttf");
+
+        user = (EditText) findViewById(R.id.user);
+        pass = (EditText) findViewById(R.id.pass);
+        signup = (TextView) findViewById(R.id.signup);
+        login = (TextView) findViewById(R.id.login);
+
+        user.setTypeface(custom_font);
+        pass.setTypeface(custom_font);
+
+        login.setTypeface(custom_font1);
+        signup.setTypeface(custom_font);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(LoginActivity.this, LoginSignupActivity.class);
+                startActivity(it);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sUser = user.getText().toString();
+                sPass = pass.getText().toString();
+
+                if (sUser.matches("admin") && sPass.matches("admin")) {
+                    Intent it = new Intent(LoginActivity.this, LookingRoomActivity.class);
+                    startActivity(it);
+                    //return;
+                } else {
+                    Toast.makeText(getApplicationContext(), "Incorrect Login!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/villanova/freespace/LookingRoomActivity.java
@@ -0,0 +1,54 @@
+package villanova.freespace;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class LookingRoomActivity extends AppCompatActivity {
+
+    Button individual;
+    Button group;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lookingroom);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("");
+        actionBar.setDisplayUseLogoEnabled(true);
+
+        individual = (Button) findViewById(R.id.individual);
+        group = (Button) findViewById(R.id.group);
+
+        //individual.setTypeface(custom_font);
+        //group.setTypeface(custom_font);
+
+        individual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    Intent it = new Intent(LookingRoomActivity.this, IndividualDashboardActivity.class);
+                    startActivity(it);
+                    //return;
+            }
+        });
+
+        group.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(LookingRoomActivity.this, GroupDashboardActivity.class);
+                startActivity(it);
+                //return;
+            }
+        });
+    }
+
+}
